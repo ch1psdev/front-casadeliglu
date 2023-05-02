cea38e5c5402b80749c93c74b3982f25de1ed451
Matias Moncada <matias.monc95@outlook.com>
2023-05-01 23:00:49 -0400

    Se agrega navbar

--- casadeliglu/src/app/components/Header.jsx
+++ casadeliglu/src/app/components/Header.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { CarritoIcon } from '../../assets/Icons'
+import logo from '../../assets/img/logo.webp'
 
 export const Header = () => {
   return (
     <>
-        
+        <div className="container-fluid">
+            <div className='row contenedor-header'>
+                <div className="col-1"></div>
+                <div className="col-2">
+                    <img src={logo} alt="Logo tienda" className='header-logo' />
+                </div>
+                <div className="col-8">
+                    <div className="row" style={{height:'100%'}}>
+                        <div className="col-3"></div>
+                        <div className="col-9">
+                        <div className='header__menu'>
+                        <NavLink
+                            to="/inicio" 
+                            className={({isActive}) => `header__menu__item ${isActive ? 'header__menu--active' : 'header__menu--non-active'}`}>
+                                <div>
+                                    Inicio
+                                </div>
+                        </NavLink>
+
+                        <NavLink
+                            to="/productos" 
+                            className={({isActive}) => `header__menu__item ${isActive ? 'header__menu--active' : 'header__menu--non-active'}`}>
+                                <div>
+                                    Productos
+                                </div>
+                        </NavLink>
+
+                        <NavLink
+                            to="/quienes-somos" 
+                            className={({isActive}) => `header__menu__item ${isActive ? 'header__menu--active' : 'header__menu--non-active'}`}>
+                                <div>
+                                    ¿Quiénes <br/>somos?
+                                </div>
+                        </NavLink>
+                        
+                        <NavLink
+                            to="/contacto" 
+                            className={({isActive}) => `header__menu__item ${isActive ? 'header__menu--active' : 'header__menu--non-active'}`}>
+                                <div>
+                                    Contacto    
+                                </div>
+                        </NavLink>
+                        
+                        <NavLink
+                            to="/" >
+                                <div>
+                                    <CarritoIcon />    
+                                </div>
+                        </NavLink>
+                        
+                    </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="col-1"></div>
+            </div>
+        </div>
     </>
   )
 }
--- casadeliglu/src/router/AppRouter.jsx
+++ casadeliglu/src/router/AppRouter.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Home } from '../app/modules/home/Home'
 import { Header } from '../app/components/Header'
+import { Productos } from '../app/modules/Productos/Productos'
+import { Nosotros } from '../app/modules/Nosotros/Nosotros'
+import { Contacto } from '../app/modules/contacto/Contacto'
 
 export const AppRouter = () => {
   return (
@@ -9,6 +12,10 @@ export const AppRouter = () => {
     <Header />
         <Routes>
             <Route path='/inicio' element={<Home />} />
+            <Route path='/productos' element={<Productos />} />
+            <Route path='/quienes-somos' element={<Nosotros />} />
+            <Route path='/contacto' element={<Contacto />} />
+
             <Route path='/' element={<Navigate to='/inicio' />} />
         </Routes>
     </>
