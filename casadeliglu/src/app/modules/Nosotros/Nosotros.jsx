aa41cbd2a08d96d6d08cd81f9847dd4f4916ca6c
Bastian Suitt <suitt.bastian@gmail.com>
2023-05-24 13:58:08 -0400

    Primera version quienes somos

--- casadeliglu/src/app/modules/Nosotros/Nosotros.jsx
+++ casadeliglu/src/app/modules/Nosotros/Nosotros.jsx
@@ -1,5 +1,4 @@
-// import salmon from '../../../assets/img/productos/salmon.jpg' 
-
+import salmon from '../../../assets/img/plato_mariscos.webp'
 export const Nosotros = () => {
   return (
     <>
@@ -9,9 +8,24 @@ export const Nosotros = () => {
       <img src={salmon} alt="" />
       https://imagen.online-convert.com/es/convertir-a-webp     sitio para convertire imagenes a webp
     */}
-
-    <div className="container-fluid">
-
+    
+    <div className="container-fluid" style={{marginTop:"50px",marginBottom:"20px"}}>
+      <div className='row'>
+        <div className='col-6'>
+          <h1 style={{textAlign:"center"}}>¿Quienes Somos?</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit porro tempora rerum! Quam dolorum consequuntur aliquam odio vitae ipsa sunt! Aperiam ducimus at voluptas hic alias atque quasi molestiae dignissimos!
+          Deleniti earum consequuntur eveniet reiciendis dolorum possimus odit necessitatibus ex consectetur vero quis, ratione, exercitationem magni perspiciatis. Sequi molestiae, quo eius minus temporibus nisi facilis necessitatibus minima consequatur deserunt harum.
+          Animi ex debitis, voluptatum nobis dolores, ut accusamus soluta, esse autem nam perspiciatis provident tempore ipsum perferendis! Omnis laudantium beatae dolore vero quod laboriosam quasi, dignissimos, explicabo ullam exercitationem sit.
+          Quidem pariatur ea possimus reiciendis tenetur nobis, quae earum! Esse dolor perferendis distinctio vel, voluptatibus deleniti! Dignissimos minus odit corrupti voluptate totam obcaecati libero, qui eius. Recusandae ad repudiandae consequatur!
+          Animi impedit accusantium, maxime hic fuga ullam assumenda temporibus maiores ipsam quae porro reprehenderit unde accusamus iure enim? Assumenda possimus odit reprehenderit aperiam sequi dignissimos corporis rem laboriosam natus omnis.
+          Consequuntur illo cupiditate ex dolores sunt consequatur eligendi veniam expedita neque atque, veritatis, sit sequi laboriosam cum fugit tempore perferendis eveniet! Repellat, facere? Unde quis autem natus, perferendis labore illum!
+          Libero assumenda, vel porro veritatis in soluta error provident consectetur dignissimos non beatae atque voluptatum odio culpa cum sed eos similique sunt dolorem? Impedit hic provident doloremque rerum eligendi voluptates.
+          Illo fuga esse incidunt. Distinctio vero laboriosam ratione hic, minus nostrum facere in sequi blanditiis laborum repudiandae vel dolorem. Quidem magnam atque odio voluptas accusantium asperiores tenetur nam quam culpa.
+          Excepturi fugiat aut autem. Impedit repudiandae ad tempora dignissimos illo quidem error ducimus aut voluptas voluptatibus omnis hic culpa eum modi expedita atque ipsum optio rerum, quia officiis fuga sint?
+          Quo tenetur quidem, eum suscipit incidunt eaque ex aliquid accusamus quas dicta, voluptas maxime velit vero dolores commodi, recusandae earum in! Aut exercitationem eligendi in accusamus! Quidem modi dolorum deserunt.</p>
+        </div>
+        <div className='col-6' style={{backgroundImage:`url(${salmon})`,height:"40vw", backgroundRepeat:"no-repeat"}}></div>
+      </div>
     </div>
     </>
   )
