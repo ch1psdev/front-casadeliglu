d1656dd10ba59d3c8712361070e163b914b8c04a
Matias Moncada <matias.monc95@outlook.com>
2023-05-24 21:34:36 -0400

    Se agrega ruta

--- casadeliglu/src/router/AppRouter.jsx
+++ casadeliglu/src/router/AppRouter.jsx
@@ -18,6 +18,7 @@ export const AppRouter = () => {
             <Route path='/contacto' element={<Contacto />} />
 
             <Route path='/' element={<Navigate to='/inicio' />} />
+            <Route path='/*' element={<Navigate to='/inicio' />} />
         </Routes>
       <Footer />
     </>
