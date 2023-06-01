e33fced7a9c536db03d6112297c1f17aa540b28e
Matias Moncada <matias.monc95@outlook.com>
2023-05-31 20:51:15 -0400

    webconfig

--- casadeliglu/src/router/AppRouter.jsx
+++ casadeliglu/src/router/AppRouter.jsx
@@ -19,6 +19,7 @@ export const AppRouter = () => {
 
             <Route path='/' element={<Navigate to='/inicio' />} />
             <Route path='/*' element={<Navigate to='/inicio' />} />
+            <Route path='*' element={<Navigate to='/inicio' />} />
         </Routes>
       <Footer /> 
     </>
